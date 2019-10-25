d9aacbbc7e18a5d18eea1f3b967cbce452b0bcae
Florian Le Bourdais <florian.s.lebourdais@gmail.com>
2019-10-25 19:20:59 +0200

    correct cepstrum estimator

--- src/pitch_tracking.py
+++ src/pitch_tracking.py
@@ -55,26 +55,31 @@ def time_domain_f0_autocorrelation(waveform, sampling_frequency):
     return 1 / candidate * sampling_frequency
 
 
-def frequency_domain_f0_cepstrum(spectrum, sampling_frequency):
+def frequency_domain_f0_cepstrum(waveform, sampling_frequency, fmin=82, fmax=640):
     """
     Cepstrum based f0 identification.
 
     For more information see https://en.wikipedia.org/wiki/Cepstrum.
 
-    :param spectrum: complex ndarray holding Fourier spectrum
+    :param waveform: ndarray containing time-domain signal
     :param sampling_frequency: float
     :return:
         - f0 - float, f0 frequency, in Hz
     """
-    log_spectrum = np.log(np.abs(spectrum) + 1e-12)
-    cepstrum = np.fft.ifft(log_spectrum)
-    abs_cepstrum = np.abs(cepstrum)
-    maxi = scipy.signal.argrelmax(abs_cepstrum)
-    f0_index = maxi[0][np.argmax(abs_cepstrum[maxi])]
-    # assuming the spectrum was computed using rfftfreq
-    df = sampling_frequency / 2 / spectrum.size
-    quefrency_vector = np.fft.fftfreq(n=log_spectrum.size, d=df)
-    f0 = 1 / quefrency_vector[f0_index]
+    # compute cepstrum
+    frame_size = waveform.size
+    windowed_signal = np.hamming(frame_size) * waveform
+    dt = 1 / sampling_frequency
+    freq_vector = np.fft.rfftfreq(frame_size, d=dt)
+    X = np.fft.rfft(windowed_signal)
+    log_X = np.log(np.abs(X))
+    cepstrum = np.fft.rfft(log_X)
+    df = freq_vector[1] - freq_vector[0]
+    quefrency_vector = np.fft.rfftfreq(log_X.size, df)
+    # extract max-peak in cepstrum in valid region
+    valid = (quefrency_vector > 1 / fmax) & (quefrency_vector <= 1 / fmin)
+    max_quefrency_index = np.argmax(np.abs(cepstrum)[valid])
+    f0 = 1 / quefrency_vector[valid][max_quefrency_index]
     return f0
 
 
@@ -89,9 +94,8 @@ def test_autocorrelation():
 def test_cepstrum():
     print('testing frequency_domain_f0_cepstrum')
     for freq in TEST_FREQS:
-        waveform = make_harmonic_wave(freq, TEST_SAMPLE_FREQ, 2048, 3)
-        spectrum = np.fft.rfft((waveform - waveform.mean()) * np.hamming(waveform.size))
-        f0 = frequency_domain_f0_cepstrum(spectrum, TEST_SAMPLE_FREQ)
+        waveform = make_harmonic_wave(freq, TEST_SAMPLE_FREQ, 2048, 40)
+        f0 = frequency_domain_f0_cepstrum(waveform, TEST_SAMPLE_FREQ)
         print(f'expected f0: {freq} - computed: {f0:.2f}')
 
 
